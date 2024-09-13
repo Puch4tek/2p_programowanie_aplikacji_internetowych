867c6326515ac87512848c06895cfada016b197e
2p01 <jakubgrzegorczyk09@gmail.com>
2024-09-13 15:50:14 +0200

    13.09

--- Zadania/Z02 - Średnia liczb/main.js
+++ Zadania/Z02 - Średnia liczb/main.js
@@ -6,6 +6,26 @@ const liczba_d = document.querySelector('#d')
 const wynik = document.querySelector('#wynik')
 const btn = document.querySelector('button')
 
-btn.addEventListener('click', function (){
-    alert('!!')
+btn.addEventListener('click', function () {
+    let a = parseInt(liczba_a.value)
+    let b = parseInt(liczba_b.value)
+    let c = parseInt(liczba_c.value)
+    let d = parseInt(liczba_d.value)
+
+    let suma = a + b + c + d
+    let srednia = suma / 4
+
+    console.log(suma)
+    console.log(srednia)
+
+    wynik.innerHTML =
+        `
+        A = <b>${a}</b><br>
+        B = <b>${b}</b><br>
+        C = <b>${c}</b><br>
+        D = <b>${d}</b><br>
+        
+        <h4>Suma = <b>${suma}</b></h4>
+        <h4>Średnia = <b>${srednia}</b></h4>
+        `
 })
