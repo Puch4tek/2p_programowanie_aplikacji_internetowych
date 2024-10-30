f29a6ea240ca9ef03ddde4725f0a442bd82b4ae5
2p01 <jakubgrzegorczyk09@gmail.com>
2024-10-30 11:23:36 +0100

    30.10

---
+++ Zadania/Z25/main.js
@@ -0,0 +1,19 @@
+const liczba_a1 = document.querySelector('#a1');
+const liczba_b1 = document.querySelector('#b1');
+const liczba_c1 = document.querySelector('#c1');
+const liczba_a2 = document.querySelector('#a2');
+const liczba_b2 = document.querySelector('#b2');
+const liczba_c2 = document.querySelector('#c2');
+const wynik = document.querySelector('#wynik');
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function () {
+    let a1 = parseFloat(liczba_a1.value());
+    let b1 = parseFloat(liczba_b1.value());
+    let c1 = parseFloat(liczba_c1.value());
+    let a2 = parseFloat(liczba_a2.value())
+    let b2 = parseFloat(liczba_b2.value())
+    let c2 = parseFloat(liczba_c2.value())
+
+
+})
