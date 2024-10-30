5d6b01fca5ac56dd21f1078cd640d736d81162bb
Puchatek <84461914+Puch4tek@users.noreply.github.com>
2024-10-30 11:34:50 +0100

    Update main.js

--- Zadania/Z25/main.js
+++ Zadania/Z25/main.js
@@ -14,6 +14,21 @@ btn.addEventListener('click', function () {
     let a2 = parseFloat(liczba_a2.value())
     let b2 = parseFloat(liczba_b2.value())
     let c2 = parseFloat(liczba_c2.value())
+    let y = (c2*a1-c1*a2)/(b2*a1 - a2*b1)
+    let x = (c1-b1*y)/(a1)
 
+    wynik.innerHTML = 
+        `
+        <p>
+        a<sub>1</sub> = ${a1}<br>
+        b<sub>1</sub> = ${b1}<br>
+        c<sub>1</sub> = ${c1}<br>
+        a<sub>2</sub> = ${a2}<br>
+        b<sub>2</sub> = ${b2}<br>
+        c<sub>2</sub> = ${c2}<br><br>
+        <b>x = ${x}</b><br>
+        <b>y = ${y}</b><br>
+        </p>
+        `
 
 })
