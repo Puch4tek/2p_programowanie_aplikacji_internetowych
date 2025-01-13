0f27cd2f776c37fd43a2fe2dc8a609a15ed25a00
2p01 <jakubgrzegorczyk09@gmail.com>
2025-01-13 13:00:26 +0100

    13.01

---
+++ Tematy/T27 - Tablice pseudolosowe/main.js
@@ -0,0 +1,18 @@
+const liczbaN = document.querySelector('#n');
+const wynik = document.querySelector('#wynik');
+const btn = document.querySelector('#btn');
+
+btn.addEventListener('click', function() {
+    let n = parseInt(liczbaN.value);
+    let tab = [];
+    for(let i = 0; i < n; i++) {
+        tab[i] = Math.random();
+    }
+    let wynikTekst = `<h3>Tablica liczb: </h3><br>`
+    for (let i = 0; i < n; i++) {
+        wynikTekst+=`tab[${i}] = ${tab[i]}<br>`;
+    }
+    let max = Math.max(...tab);
+    wynikTekst+=`<b>Maksymalna wartość: </b> ${max} <br>`;
+    wynik.innerHTML = wynikTekst;
+})
