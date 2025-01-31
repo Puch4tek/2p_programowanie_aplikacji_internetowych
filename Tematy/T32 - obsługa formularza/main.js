dc47c81ccd94a620d7d1067a445076ce32b0b5d9
2p01 <jakubgrzegorczyk09@gmail.com>
2025-01-31 13:16:36 +0100

    31.01

--- Tematy/T32 - obsługa formularza/main.js
+++ Tematy/T32 - obsługa formularza/main.js
@@ -1,27 +1,20 @@
-/*
-Zadanie polega na stworzeniu aplikacji JavaScript według wzoru pokazanego na grafice. Zwróć uwagę, że nagłówek h1 "Ustalenie wartości za pomocą suwaka" - zmienia kolor na taki, jaki został wybrany przez użytkownika w polu "Ocena kolorem". Również łańcuch znaków "ocena kolorem", powinien być zapisany w wybranym kolorze.*/
+const dateInput = document.querySelector('#dateInput');
+const dateOutput = document.querySelector('#dateOutput');
+const range = document.querySelector('#range');
+const color = document.querySelector('#color');
+const grade = document.querySelector('#grade');
 
-const wynik = document.querySelector('#wynik');
-
-const h1 = document.createElement('h2');
-h1.textContent = 'Ustalenie wartości za pomocą suwaka';
-wynik.appendChild(h1);
-
-const input = document.createElement('input');
-input.setAttribute('type', 'range');
-input.setAttribute('min', '0');
-input.setAttribute('max', '255');
-input.setAttribute('value', '0');
-wynik.appendChild(input);
-
-const div = document.createElement('h2');
-div.textContent = 'ocena kolorem';
-wynik.appendChild(div);
-
-input.addEventListener('input', () => {
-    div.style.color = `rgb(${input.value}, 0, 0)`;
-    div.textContent = `ocena kolorem`;
-
+color.addEventListener('input', () => {
+    console.log(color.value);
+    document.querySelector('#h1').style.color = color.value;
+    document.querySelector('#colorGrade2').style.color = color.value;
 });
 
+dateInput.addEventListener('input', () => {
+    const date = new Date(dateInput.value);
+    dateOutput.textContent = date.toISOString().slice(0, 10);
+});
 
+range.addEventListener('input', () => {
+    grade.textContent = range.value.toString();
+});
