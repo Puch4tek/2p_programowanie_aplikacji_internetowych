8ffc24ca112f617fbe37d83dd3294d62551e6676
Puch4tek <jakubgrzegorczyk09@gmail.com>
2025-02-03 23:13:43 +0100

    03.02

--- Tematy/T32b - cena strzyżenia/main.js
+++ Tematy/T32b - cena strzyżenia/main.js
@@ -1,17 +1,3 @@
-/*/Utwórz stronę html o nazwie fryzura.html. Strona powinna posiadać następującą zawartość:
-
-
-
-Wymagania dotyczące skryptu:
-
-    Napisany w języku wykonywanym po stronie przeglądarki
-Uruchamia się po wciśnięciu przycisku na stronie fryzura.html
-Podaje cenę strzyżenia w zależności od wyboru dokonanego przez pola radio zgodnie z cennikiem w tabeli
-        ‒ Wyświetla wynik działania pod przyciskiem według wzoru: „Cena strzyżenia: <wartość>”, gdzie <wartość> oznacza cenę strzyżenia dla wskazanej długości włosów.
-    Długość włosów/ Cena
-    Krótkie 25 | Średnie 30 | Półdługie 40 | Długie 50    </p.>*/
-
-
 // Funkcja obliczająca cenę strzyżenia
 function obliczCeneStrzyzenia(dlugosc) {
     let cena;
