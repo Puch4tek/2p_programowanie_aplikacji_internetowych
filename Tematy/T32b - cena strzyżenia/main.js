6f5267905609ac2857138e8d639aac7cd66e5a52
2p01 <jakubgrzegorczyk09@gmail.com>
2025-02-17 13:21:17 +0100

    17.02

--- Tematy/T32b - cena strzyżenia/main.js
+++ Tematy/T32b - cena strzyżenia/main.js
@@ -1,26 +1,34 @@
+const selectedRadio = document.querySelector('input[name="dlugosc"]:checked');
+
 // Funkcja obliczająca cenę strzyżenia
 function obliczCeneStrzyzenia(dlugosc) {
     let cena;
     switch (dlugosc) {
-        case "krótkie":
+        case "krotkie":
             cena = 25;
             break;
-        case "średnie":
+        case "srednie":
             cena = 30;
             break;
-        case "półdługie":
+        case "poldlugie":
             cena = 40;
             break;
-        case "długie":
+        case "dlugie":
             cena = 50;
             break;
+        default:
+            cena = 0; // Default case to handle unexpected values
     }
     return cena;
 }
 
 // Przypisanie zdarzenia do przycisku "Strzyżenie"
-document.getElementById('strzyżenie').addEventListener('click', function() {
-    let dlugosc = document.querySelector('input[name="dlugosc"]:checked').value;
-    let cena = obliczCeneStrzyzenia(dlugosc);
-    document.getElementById('wynik').textContent = `Cena strzyżenia: ${cena}`;
+document.getElementById('strzyzenie').addEventListener('click', function() {
+    if (selectedRadio) {
+        let dlugosc = selectedRadio.value;
+        let cena = obliczCeneStrzyzenia(dlugosc);
+        document.getElementById('wynik').textContent = `Cena strzyżenia: ${cena}`;
+    } else {
+
+    }
 });
